4536546e668a8d5bac71d42a5614c3afeaaf27df
Pushkar Raj <akashkumar5076hero@gmail.com>
2026-02-01 04:06:08 +0530

    Enable Swagger in production

--- backend/AppointmentAPI/Program.cs
+++ backend/AppointmentAPI/Program.cs
@@ -229,12 +229,22 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+//     // app.MapOpenApi();
+// }
+
+//---After backend deploy on Railway
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    // app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Appointment API v1");
+    c.RoutePrefix = "swagger";
+});
+
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
